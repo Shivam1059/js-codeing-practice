ee71fad06b0615e98e30462e257800a22540cf67
Shivam Ahirwar <144521851+Shivam1059@users.noreply.github.com>
2024-02-12 17:23:51 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "shivam@gmail.com"
+var accountPassword = "2345"
+accountCity = "Jaipur"
+
+//accountId = 2
+accountEmail = "shiba@gamil.com"
+accountPassword = "6543"
+accountCity = "Bengluru"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
