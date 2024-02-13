f513c35546fc21d2bb4d32c6dc16709ace441d66
Shivam Ahirwar <144521851+Shivam1059@users.noreply.github.com>
2024-02-14 00:04:30 +0530

    Add files via upload

---
+++ conversion-operator.js
@@ -0,0 +1,58 @@
+// let score = "33"
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+//"33abc" => NaN 
+// true => 1; false => 0
+/*
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+*/
+// 1 => true
+// 2 => false
+// " " => false
+//"shivam" => true
+
+// let someNumber = 44;
+
+// let stringNumber = String(someNumber);
+// console.log(typeof stringNumber);
+
+//*****************************************operation*********************************//
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2%3);
+// console.log(2*2);
+// console.log(2**2);
+
+
+
+
+let str1 = "hello"
+let str2 = " shivam"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log('1' + 2 + 3);
+console.log( 1 + 2 + "3");
+
+// if first element are string so all next element consider as string
+// if first element are intiger so all next element consider as intiger
+
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
