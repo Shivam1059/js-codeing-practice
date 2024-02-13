5ddfbaa3df92948186448043e32bae889f9bc12d
Shivam Ahirwar <144521851+Shivam1059@users.noreply.github.com>
2024-02-13 15:13:37 +0530

    Add files via upload

---
+++ datatype .js
@@ -0,0 +1,15 @@
+"use strict"; // treat all js code as newer versions 
+
+// alert (3 + 3 )// we are using nodejs not browse
+
+console.log(3+3);
+
+ 
+ let name = "shivam";
+ let age = 23
+ let isLoggedIn = false
+
+ console.log(typeof null);//object
+ console.log(typeof undefined)//undefined
+
+ 
