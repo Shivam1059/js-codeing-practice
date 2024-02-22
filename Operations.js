eceb1691410432e32f0b0722fdfaa8eaaf9dee51
Shivam Ahirwar <144521851+Shivam1059@users.noreply.github.com>
2024-02-23 00:07:50 +0530

    Add files via upload

---
+++ Operations.js
@@ -0,0 +1,30 @@
+// operation in js 
+
+a = 10
+b = 15
+
+//addition 
+sum = a + b 
+console.log(sum);
+
+//substruection
+
+diff = a - b
+console.log(diff);
+
+// multification
+prod = a * b
+console.log(prod);
+
+//divition
+div = a / b
+console.log(div);
+
+//moduls
+rem =a % b
+console.log(rem);
+
+//Exponentiation(power operator)
+ 2**3
+ console.log(2**3);
+
