2c1107192c4a7ea994fb749ab34382e91fe3f0a4
Shivam Ahirwar <144521851+Shivam1059@users.noreply.github.com>
2024-02-24 09:04:05 +0530

    Add files via upload

---
+++ unary operator.js
@@ -0,0 +1,29 @@
+// increment operator
+
+let year = 2000;
+ year = year++; //post
+console.log(year);
+
+let age = 17;
+++age //pre
+console.log(age);
+
+// dicrement operator
+
+let votingAge= 18;
+age = -- votingAge;  //pre
+console.log(age);
+
+let Number = 99;
+Number = Number--; //post
+console.log(Number)
+
+let num = 5;
+let newNum = num++;
+newNum = ++num;
+console.log(newNum)
+
+age = 7 ;
+newAge = age ++;
+newAge = ++age;
+console.log(age);
