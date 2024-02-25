a59c5ce7567fdfe806d69c04cb0f99115022b538
Shivam Ahirwar <144521851+Shivam1059@users.noreply.github.com>
2024-02-25 23:00:10 +0530

    Add files via upload

---
+++ prectice_question2.js.js
@@ -0,0 +1,35 @@
+//Arithmetic Operators
+
+let a = 5;
+let b = 10;
+
+console.log(a+b);
+console.log(b-a);
+console.log(a*b);
+console.log(a**b);
+console.log(a/b);
+console.log(a%b);
+
+//unary opertor
+
+console.log(a++);
+console.log(++a);
+
+console.log(b--);//post unary operators
+console.log(--b);//pre unary operators
+
+
+//compare opertors
+let age = 18;
+let vote = age;
+console.log(age >=18);
+
+
+console.log(123 == "123")
+console.log(123 === "123")
+
+//comparison for non-numbers
+'a'>'A'
+console.log('a'<'A');
+console.log('b'>'b');
+console.log('B'<'C')
