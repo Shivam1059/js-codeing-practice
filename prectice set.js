57ec85c280603b76d81c14f848eeb0b129f437f9
Shivam Ahirwar <144521851+Shivam1059@users.noreply.github.com>
2024-02-25 22:57:28 +0530

    Add files via upload

---
+++ prectice set.js
@@ -0,0 +1,12 @@
+let colour = "green";
+
+//Traffic light System
+if( colour === "red"){
+  console.log("Stop.light color is red");
+}
+if( colour === "yellow"){
+  console.log("ready for drive.light color is yellow");
+}
+if( colour === "green"){
+  console.log("go.light color is green");
+}
