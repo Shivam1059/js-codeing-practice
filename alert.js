ffe5b6df1bd69d2f05da630d55bdeb8c2276e050
Shivam Ahirwar <144521851+Shivam1059@users.noreply.github.com>
2024-02-27 22:15:53 +0530

    Add files via upload

---
+++ alert.js
@@ -0,0 +1,19 @@
+alert("work is progress ");
+console.log("something is wrong");
+console.error("this is wrong msg");
+console.warn("this is a waring msg");
+
+prompt("enter you name");
+
+
+let Name = prompt("enter your first name");
+let age = prompt("enter your second name");
+alert(`${Name} is ${age} years old.`);
+
+
+let num = 77;
+if( num % 10 == 0){
+  console.log("good");
+}else{
+  console.log("bad");
+}
