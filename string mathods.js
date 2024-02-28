27ef35aa74ccd3b95aae724af32ab42b57e32c18
Shivam Ahirwar <144521851+Shivam1059@users.noreply.github.com>
2024-02-28 23:25:41 +0530

    Add files via upload

---
+++ string mathods.js
@@ -0,0 +1,82 @@
+/*
+//trim mathods
+
+//question 1
+let msg = "  Hello ";
+msg.trim();
+console.log(msg.trim());
+
+//question 2
+let password = "  namaste-india ";
+ newpass = password.trim();
+console.log(newpass);
+
+// string change toupercase 
+
+//question 1
+let str = "good morning all of you"
+str.toUpperCase();
+console.log(str.toUpperCase());
+
+//question 2
+let army = "indian army is the world most repectable army";
+army.toUpperCase();
+console.log(army.toUpperCase());
+
+//string change in tolowercase
+
+//question 1
+let god = "RAM JI";
+ god.toLowerCase();
+console.log(god.toLowerCase());
+
+//question 2
+let rivers = "GANGA, YAMUNA , NARMDA";
+rivers.toUpperCase();
+console.log(rivers.toUpperCase());
+
+//string methods with arguments
+//question 1
+let code = "ILoveCoding";
+code.indexOf("C");
+console.log(code.indexOf("C"));
+
+//question 2
+let day = "sunday is funday";
+day.indexOf("f");
+console.log(day.indexOf("f"));
+
+
+//method chaning
+
+//question 1
+
+let massage = "  hello  ";
+let newMassage = massage.trim();
+console.log(newMassage);
+newMassage = newMassage.toUpperCase();
+console.log(newMassage);
+
+//short way 
+
+let product = "   made in india ";
+let newProduct = product.trim().toUpperCase();
+console.log(newProduct);
+
+*/
+//slice methods
+let Str = "ILoveCoding";
+Str.slice(5,8);
+console.log(Str.slice(5,8));
+
+
+Str.slice(1,5).toUpperCase();
+console.log(Str.slice(1,5).toUpperCase()
+);
+
+Str.slice(-6);
+console.log(Str.slice(-6));
+
+
+
+
