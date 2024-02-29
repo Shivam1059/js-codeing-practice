a1ad339da98e0ad29a35995230b84406f8326cdf
Shivam Ahirwar <144521851+Shivam1059@users.noreply.github.com>
2024-02-29 23:45:48 +0530

    Add files via upload

---
+++ prectices-question.js
@@ -0,0 +1,24 @@
+//proble 1
+
+let msg = " help!";
+msg.trim().toUpperCase();
+console.log(msg.trim().toUpperCase());
+
+//proble 2
+let name = "ApnaCollege";
+name.slice(4,9);
+console.log(name.slice(4,8));
+
+name.indexOf("na");
+console.log(name.indexOf("na"));
+
+name.replace("Apna","our");
+console.log(name.replace("Apna","Our"));
+
+inst = name.slice(4);
+console.log(name.slice(4));
+
+news= inst.replace("l","t");
+console.log(inst.replace("l","t"));
+
+console.log(news.replace("l","t"));
