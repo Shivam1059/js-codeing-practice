c3cc06f82f26d77ac2bcfed9fe1fa8a3dca8b46e
Shivam Ahirwar <144521851+Shivam1059@users.noreply.github.com>
2024-03-02 22:14:00 +0530

    Add files via upload

---
+++ array operation.js
@@ -0,0 +1,20 @@
+
+//array on opertion
+
+//Short 
+const array = [1,9,2, 3, 65, 5, ];
+array.sort();
+console.log(array);
+
+//push 
+const arr = [1,4,6,8,11,34,56];
+arr.push("a")
+console.log(arr);
+
+//revers
+const box = [ 1,12,34,56,78,98];
+box.reverse()
+console.log(box);
+
+
+
