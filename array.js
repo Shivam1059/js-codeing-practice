4dec83df4bd7831d04331be4458f21885ac82526
Shivam Ahirwar <144521851+Shivam1059@users.noreply.github.com>
2024-03-02 22:15:25 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,30 @@
+//arrays 
+
+//question 1
+let students = ["shivam","nikita","aman","shradha"];
+console.log(students.length);
+console.log(students);
+
+//question 2
+let branch = ["CS","IT","MC","CE","EC","AIDS"];
+console.log(branch);
+console.log(branch.length);
+console.log(branch[4]);
+
+//question 3  //push operation
+let marks = [44,54,65,78,98,76,56,87,99,78];
+marks.push(88);
+console.log(marks);
+
+//question 4 //pop & sort operation
+let num = [45,76,87,98,66,43,65,86,55];
+num.pop(4);
+num.sort();
+console.log(num);
+
+//question 5 mixed array
+let info = ["shivam",19,"IT",82.20];
+console.log(info);
+console.log(info.length);
+console.log(info[0]);
+console.log(info[0][3]);
