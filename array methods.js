85773940370359c5cf03a08e518fd4a42024eafd
Shivam Ahirwar <144521851+Shivam1059@users.noreply.github.com>
2024-03-04 01:00:41 +0530

    Add files via upload

---
+++ array methods.js
@@ -0,0 +1,48 @@
+// question 1  indexof methods
+let primery = [ "red","yellow","blue","pink"];
+primery.indexOf("yellow");
+console.log(primery.indexOf("yellow"));
+console.log(primery.indexOf("pink"));
+//includes methods
+primery.reverse();
+console.log(primery.reverse());
+
+console.log(primery.includes("blue"));
+
+//question2 concat methods
+let first = ["mango","orange","banana"];
+let second = ["apple","grepsh","papaya"];
+fruts = first.concat(second);
+console.log(fruts);
+
+// question 3 reverse methods
+let cars = ["maruti","toyota","xuv","scarpuo"];
+car = cars.reverse();
+console.log(car);
+let luxary = ["bmw","tesla","applecar","ferari","royls royal"];
+luxarycar = luxary.reverse();
+console.log(luxarycar);
+
+//question 4 slice methods
+let colors = ["red","blue","black","orange","white","pink"];
+colors.slice();
+console.log(colors.slice(2,5));
+console.log(colors.slice(3));
+
+//question 5 
+let color = ["red" ,"yellow","blue","orange","pink","white"];
+colors.slice();
+console.log(color);
+console.log(color.slice(2));
+console.log(color.slice(2,3));
+console.log(color.slice(-2));
+
+//question 6 splice
+let reambow = ["blue","red","orange","pink","white","yellow"];
+reambow.splice(1,5);
+console.log(reambow);
+
+//question 7 sort
+let car = ["ferrari","toyota","xuv","tesla","scorpuo"];
+car.short();
+console.log(car.short());
